3e14a95e79ffe9a9ca3dadc33139c9662764bd36
suryatejaadluri <suryatejaadluri@gmail.com>
2017-06-06 13:22:20 +0530

    Changing code after resolving issue no.1

--- 1.RWD JAVASCRIPT/1.Loginpage/formvalidation.js
+++ 1.RWD JAVASCRIPT/1.Loginpage/formvalidation.js
@@ -1,12 +1,16 @@
+var err=0;
 function user()
 {
 var x=document.getElementById('username').value;
 if(x=="")
 {
 document.getElementById('er1').style.visibility='visible';
+err=1;
 
-} else                        
+} else  {                      
 document.getElementById('er1').style.visibility='hidden';
+err=0;
+}
 }
 
 function Emailv(){
@@ -16,9 +20,11 @@ var dotpos = y.lastIndexOf(".");
 if (atpos<1|| dotpos<atpos+2 || dotpos+2>=y.length) {
 
 document.getElementById('er2').style.visibility='visible';
+err=1;
 }
-else document.getElementById('er2').style.visibility='hidden';
-}
+else {document.getElementById('er2').style.visibility='hidden';
+err=0;
+}}
 
 function passv(){
 var z=document.getElementById('password').value;
@@ -26,6 +32,7 @@ if(z=="")
 {       
 document.getElementById('er3').style.visibility='visible';
 document.getElementById('er3').innerHTML='please enter your password';
+err=1;
 
 }
 
@@ -33,15 +40,25 @@ else if(z.length>=10||z.length<4)
 {
 document.getElementById('er3').style.visibility='visible';
 document.getElementById('er3').innerHTML='password length should btw 4 to 10';
+err=1;
 
 }           
-else document.getElementById('er3').style.visibility='hidden';
-}
+else 
+{
+  err=0;
+  document.getElementById('er3').style.visibility='hidden';
+}}
 function validate()
 {
+
 user();
 Emailv();
 passv();
+
+if(err==0)
+{
+alert('Form is succesfully submitted');
+}
 document.getElementById("myForm").reset();  
 return false;
 }
