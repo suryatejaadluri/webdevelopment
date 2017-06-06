ba706f241a94efdc9161088bd9ded8982c5785fa
suryatejaadluri <suryatejaadluri@gmail.com>
2017-06-06 18:35:20 +0530

    Resolving issue no.2

--- 1.RWD JAVASCRIPT/2.DynTable/table_script.js
+++ 1.RWD JAVASCRIPT/2.DynTable/table_script.js
@@ -40,7 +40,8 @@ function add_row()
 var new_name=document.getElementById("new_name").value;
 var new_country=document.getElementById("new_country").value;
 var new_age=document.getElementById("new_age").value;
-
+if(new_age!=""&&new_country!=""&&new_name!="")
+    {
 var table=document.getElementById("data_table");
 var table_len=(table.rows.length)-1;
 var row = table.insertRow(table_len).outerHTML="<tr id='row"+table_len+"'><td id='name_row"+table_len+"'>"+new_name+"</td><td id='country_row"+table_len+"'>"+new_country+"</td><td id='age_row"+table_len+"'>"+new_age+"</td><td><input type='button' id='edit_button"+table_len+"' value='Edit' class='edit' onclick='edit_row("+table_len+")'> <input type='button' id='save_button"+table_len+"' value='Save' class='save' onclick='save_row("+table_len+")'> <input type='button' value='Delete' class='delete' onclick='delete_row("+table_len+")'></td></tr>";
@@ -48,4 +49,9 @@ document.getElementById("save_button"+table_len).style.display="none";
 document.getElementById("new_name").value="";
 document.getElementById("new_country").value="";
 document.getElementById("new_age").value="";
+}
+else
+    {
+        alert('do not add empty data to the table');
+    }
 }
