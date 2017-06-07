65b3818c6412ffc680b287999fcca607beccfa56
suryatejaadluri <suryatejaadluri@gmail.com>
2017-06-07 16:59:32 +0530

    Resolving issue no.4 todo list is now working properly

--- 1.RWD JAVASCRIPT/4.TODO List/scripty.js
+++ 1.RWD JAVASCRIPT/4.TODO List/scripty.js
@@ -1,7 +1,45 @@
-function insert()
+function get_todos() {
+    var todos = new Array;
+    var todos_str = sessionStorage.getItem('list');
+    if (todos_str !== null) {
+        todos = JSON.parse(todos_str); 
+    }
+    return todos;
+};
+
+
+function add()
 {
     
-var data=document.getElementById("databox").value;
-document.getElementById("test").style.display="none";
-return false;
+var todo=document.getElementById("databox").value;
+if(todo=="")
+{
+    alert("please enter your todo");
 }
+    else{
+var todos=get_todos();
+todos.push(todo);
+
+sessionStorage.setItem("list",JSON.stringify(todos));
+show();
+    
+
+return false;
+}};
+
+function show()
+{
+var todos=get_todos();
+
+var html="<ul>";
+    
+    for(i=0;i<todos.length;i++)
+        {
+            html+="<li>"+todos[i]+"</li>";
+        }
+    html+="</ul>";
+    document.getElementById("test").innerHTML=html;
+    
+
+
+};
